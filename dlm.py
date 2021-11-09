6a96112db066d51b56872548b0c9438f5211e6b4
Levi Ryffel <levi.ryffel@gmail.com>
2021-11-09 09:48:15 +0100

    implemented sigma and fail

--- dlm.py
+++ dlm.py
@@ -1,3 +1,5 @@
+from z3 import Int, Implies
+
 # the set of initial subterms of a set of monoid terms
 def initial_subterms(terms):
     subterms = set()
@@ -6,4 +8,33 @@ def initial_subterms(terms):
             subterms.add(t[:i])
     return subterms
 
+# the set of implications u <= v ==> ux <= vx
+# for ux != vx in terms.
+# because there is no patterm matching,
+# we do slicing, and what is `u` in the instructions
+# is now `u[:-1]`, and what is `ux` is now `u`
+def sigma(terms):
+    sig = set()
+    # would use `pairwise`, but this is 3.10
+    # for u, v in pairwise(terms):
+    for u in terms:
+        for v in terms:
+            if u == '' or v == '' or u == v:
+                pass
+            elif u[-1] == v[-1]:
+                sig.add(Implies(Int(u[:-1]) <= Int(v[:-1]), 
+                                   Int(u) <= Int(v)))
+    return sig
+
+# the set of inequations s > t for
+# s in terms1 and t in terms2.
+# if this is satisfiable together with sigma,
+# then the equation Meet(terms1) <= Join(terms2)
+# is not valid.
+def fail(terms1, terms2)
+    fail = set()
+    for s in terms1:
+        for t in terms2:
+            fail.add(Int(s) > Int(t))
+    return fail
 
--- test_dlm.py
+++ test_dlm.py
@@ -1,17 +1,42 @@
 import unittest
 from dlm import *
+from z3 import Int, Implies
+
+s1 = 'x'
+s2 = 'y'
+s3 = 'xz'
+t1 = 'zy'
+t2 = 'zz'
+
+terms = set([s1, s2, s3, t1, t2])
 
 class TestDLM(unittest.TestCase):
     def test_initial_subterms(self):
-        s1 = 'x'
-        s2 = 'y'
-        s3 = 'xz'
-        t1 = 'zy'
-        t2 = 'zz'
-
-        terms = set([s1, s2, s3, t1, t2])
         self.assertEqual(initial_subterms(terms),
                          set(['', 'x', 'y', 'xz', 'z', 'zy', 'zz']))
+
+    def test_sigma(self):
+        expected = set()
+        # x: nothing
+        # y: {uy = s2, vy = t1}, {uy = t1, vy = s2}
+        expected.add(Implies(
+            Int('') <= Int('z'),
+            Int('y') <= Int('zy')
+        ))
+        expected.add(Implies(
+            Int('z') <= Int(''),
+            Int('zy') <= Int('y')
+        ))
+        # z: {uz = s3, vz = t2}, {uz = t2, vz = s3}
+        expected.add(Implies(
+            Int('x') <= Int('z'),
+            Int('xz') <= Int('zz')
+        ))
+        expected.add(Implies(
+            Int('z') <= Int('x'),
+            Int('zz') <= Int('xz')
+        ))
+        self.assertEqual(sigma(terms), expected)
 
 if __name__ == '__main__':
     unittest.main()
